76c3327c2773486a96869307e3b242764d47c197
Giovana Rodrigues <giovana.rodrigues.silva.15@gmail.com>
2024-06-09 19:25:39 +0000

    styled: posicionamento do card no centro da tela

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,7 @@ export const theme = {
     homeBackground: "#131518",
     cardGradient: "radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%)",
     textColor: "#969FAD",
-    subimitbuttonColor: "#FC7614",
+    subimitButtonColor: "#FC7614",
     whith: "#FFF",
     feedbackButtonColor:"#262E38",
     feedbackButtonText: "#7C8798",
